0e4a6ae7943ecff8998a1956297ac4d268670aab
Jason Downing <jason_downing@student.uml.edu>
2015-09-08 18:15:14 -0400

    outputting mantissa, exponent and sign, now to convert to binary.

--- A1/a1.c
+++ A1/a1.c
@@ -5,33 +5,86 @@
  *
  */
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, char* argv[])
-{
-    // We should insure the user supplies us with at least one  floating point number.
-    if (argc < 2) {
+/*  Union to use for the conversion.
+    This post pointed me in the right direction:
+    https://stackoverflow.com/questions/15685181/how-to-get-the-sign-mantissa-and-exponent-of-a-floating-point-number
+*/
+typedef union {
+    float test;
+    struct {
+        unsigned int   mantissa: 23;
+        unsigned int exponent:   8;
+        unsigned int          sign:  1;
+    } part;
+} float_convert;
 
-        // If they didn't enter a floating point number, we will remind them to.
-        // argv[0] should be the program's name, according to Google.
-        // See: http://www.cprogramming.com/tutorial/c/lesson14.html for details.
-        printf("** Error - no floating point numbers entered. **\n");
-        printf("Usage: %s 123.123 123.134 ...\n", argv[0]);
-    }
+// Function to convert the input into readable output.
+void print_output(float output);
 
-    // If we get here, we can start doing stuff with the floating point number the user entered.
-    // Let's just print it out for now to see if this works so far.
-    // Seems like argv[1] to argv[argc] should be our floating point numbers.
-    int x;
+// Function to output to binary.
+void output_binary(unsigned int x);
 
-    /*  This will be the main loop that goes through all of the input and figures out the following:
-         mantissa:
-         exponent:
-                  sign:
+int main(int argc, char* argv[]) {
+    float input;
+    int valid = 0;
 
-    */
-    for(x = 1; x < argc; x++) {
-        printf("This is what you entered: %s\n", argv[x]);
-    }
+    printf("Enter a float(s): ");
+
+    // Get input until we find a newline ('\n')
+    do {
+        valid = scanf("%f", &input);
+        print_output(input);
+    } while (valid == 1);   // scanf will return 1 if it gets a valid input, 0 if it gets invalid input and -1 on EOF.
+
+    //TODO: make program quit when input is done.
 
     return 0;
+}
+
+
+void print_output(float output) {
+
+    // Use the union to print out the mantissa / exponent / sign.
+    float_convert in;
+    in.test = output;
+
+    // might not need this after all.
+    /*char buffer_mantissa[24];
+    char buffer_exponent[9];
+    char buffer_sign[2];
+    char buffer_base10[33];
+    */
+
+    // TODO: check output and compare.
+    // TODO: convert to binary some how.
+
+    /*
+        Note - decided to line the lines up correctly, so I found an easier way to do so using Stackoverflow.
+        (although I'm aligning right not left)
+        https://stackoverflow.com/questions/1809399/c-how-to-justify-output-lines-using-printf-to-get-equal-length
+    */
+
+    printf("The floating point value for %.1f is broken out as: \n", output);
+
+    printf("%12s %-10x %10s ", "mantissa:", in.part.mantissa, "or:");
+    output_binary(in.part.mantissa);        // TODO: make a function that outputs an int as binary.
+
+    printf("%12s %-10x %10s ", "exponent:", in.part.exponent, "or:");
+    output_binary(in.part.exponent);        // TODO: make a function that outputs an int as binary.
+
+    printf("%12s %-10x %10s ", "sign:", in.part.sign, "or:");
+    output_binary(in.part.sign);        // TODO: make a function that outputs an int as binary.
+
+    printf("%12s %-10f %10s ", "in base 10:", output, "or:");
+    output_binary(output);        // TODO: make a function that outputs an int as binary.
+
+    return;
+}
+
+// TODO: write this function.
+void output_binary(unsigned int x) {
+
+    printf("100 0000 0000\n");
 }
