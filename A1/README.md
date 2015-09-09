5c04cc340f4d16ac32e84623c6e77c74c92afb6f
Jason Downing <jdown1994@gmail.com>
2015-09-08 22:36:58 -0400

    output to binary works, fixed random spacing issues. updated README, added more numbers to test.

--- A1/README.md
+++ A1/README.md
@@ -1,3 +1,38 @@
 # Assignment 1 - Floating Point Representation
 
 Assignment 1 for Computer Architecture.
+
+This programs reads any number of floating point numbers from standard input, and outputs the following information:
+mantissa
+exponent
+sign
+base 10 equivalent
+
+There are two functions I created to make the program easy to read:
+
+void print_output(float output);
+
+This function made it easier to output the mantissa / exponent / sign / base 10 equivalent. It also contains the alignment
+code which makes all the values line up nicely, either left aligned or right aligned depending on the column.
+
+void output_binary(int integer_input);
+
+I made an int to binary conversion function since C doesn't apparently have one built in. A stackoverflow post gave me
+the idea for the function, since itoa (http://www.cplusplus.com/reference/cstdlib/itoa/) isn't valid ANSI-C. I also added some
+spacing so that the bits are grouped by 4s and I also removed the leading zeros from the output.
+
+I used a union to get the mantissa / exponent and sign, as I included a struct with several unsigned integers inside of the union.
+
+TODO:
+
+1. Test program against 10 numbers, including the following:
+
+237.75
+-.0000005126
+-92457321.670245
+6.023E+23
+1.67339E-40
+
+Test by running the following command:
+
+./a1 < test.in
--- A1/a1.c
+++ A1/a1.c
@@ -10,35 +10,37 @@
 /*  Union to use for the conversion.
     This post pointed me in the right direction:
     https://stackoverflow.com/questions/15685181/how-to-get-the-sign-mantissa-and-exponent-of-a-floating-point-number
+
+    Also, the "convert_float_to_bits.c" file gave me some ideas as well:
+    https://jeapostrophe.github.io/courses/2015/fall/305/notes/dist/convert_float_to_bits.c
 */
-typedef union {
-    float test;
+union float_32 {
+    float float_value;
     struct {
-        unsigned int   mantissa: 23;
-        unsigned int exponent:   8;
+        unsigned int mantissa: 23;
+        unsigned int exponent:  8;
         unsigned int          sign:  1;
     } part;
-} float_convert;
+} float_32;
 
 // Function to convert the input into readable output.
 void print_output(float output);
 
 // Function to output to binary.
-void output_binary(unsigned int x);
+void output_binary(int integer_input);
 
 int main(int argc, char* argv[]) {
     float input;
     int valid = 0;
 
-    printf("Enter a float(s): ");
-
     // Get input until we find a newline ('\n')
     do {
-        valid = scanf("%f", &input);
+        printf("Enter a float(s): ");
+        valid = scanf(" %f", &input);
         print_output(input);
-    } while (valid == 1);   // scanf will return 1 if it gets a valid input, 0 if it gets invalid input and -1 on EOF.
-
-    //TODO: make program quit when input is done.
+    // scanf will return 1 if it gets a valid input, 0 if it gets invalid input and -1 on EOF.
+    // It should separate inputs by ignoring newlines and quit whenever it hits EOF.
+    } while (valid == 1);
 
     return 0;
 }
@@ -47,18 +49,9 @@ int main(int argc, char* argv[]) {
 void print_output(float output) {
 
     // Use the union to print out the mantissa / exponent / sign.
-    float_convert in;
-    in.test = output;
-
-    // might not need this after all.
-    /*char buffer_mantissa[24];
-    char buffer_exponent[9];
-    char buffer_sign[2];
-    char buffer_base10[33];
-    */
+    float_32.float_value = output;
 
     // TODO: check output and compare.
-    // TODO: convert to binary some how.
 
     /*
         Note - decided to line the lines up correctly, so I found an easier way to do so using Stackoverflow.
@@ -68,23 +61,79 @@ void print_output(float output) {
 
     printf("The floating point value for %.1f is broken out as: \n", output);
 
-    printf("%12s %-10x %10s ", "mantissa:", in.part.mantissa, "or:");
-    output_binary(in.part.mantissa);        // TODO: make a function that outputs an int as binary.
+    printf("%12s 0x%-8x %10s ", "mantissa:", float_32.part.mantissa, "or:");
+    output_binary(float_32.part.mantissa);
 
-    printf("%12s %-10x %10s ", "exponent:", in.part.exponent, "or:");
-    output_binary(in.part.exponent);        // TODO: make a function that outputs an int as binary.
+    printf("%12s 0x%-8x %10s ", "exponent:", float_32.part.exponent, "or:");
+    output_binary(float_32.part.exponent);
 
-    printf("%12s %-10x %10s ", "sign:", in.part.sign, "or:");
-    output_binary(in.part.sign);        // TODO: make a function that outputs an int as binary.
+    printf("%12s %-10x %10s ", "sign:", float_32.part.sign, "or:");
+    output_binary(float_32.part.sign);
 
     printf("%12s %-10f %10s ", "in base 10:", output, "or:");
-    output_binary(output);        // TODO: make a function that outputs an int as binary.
+    output_binary(output);
 
     return;
 }
 
-// TODO: write this function.
-void output_binary(unsigned int x) {
+/*  TODO: test this function.
+    Idea from this post, but I changed it to a char array to make it remove leading zeroes.
+https://stackoverflow.com/questions/19823359/represent-unsigned-integer-in-binary
+*/
+void output_binary(int integer_input) {
+    char bit_array[32];
+    int x, count = 1, k = 1;
 
-    printf("100 0000 0000\n");
+    // Initalize everything to zero.
+    for (x = 0; x < 32; x++) {
+        bit_array[x] = '0';
+    }
+
+    // Convert to 1s and 0s, spacing everything out in groups of 4.
+    for (x = 0; x < 32; x++) {
+        if (integer_input & k) {        // Bitwise AND operation.
+            bit_array[x] = '1';
+        }
+        else {
+            bit_array[x] = '0';
+        }
+        k = k << 1;                         // Left shift by one place each time.
+    }
+
+    // Hack: remove leading zeroes by replacing them with spaces!
+    for (x = 31; x >= 0; x--) {
+        if(bit_array[x] == '0') {                   // Just remove any zeroes we find!
+            bit_array[x] = ' ';
+        }
+        else if (bit_array[x] == '1') {         // And as soon as we find a 1, quit the loop!
+            break;
+        }
+
+        // But, what if we removed the only zero?
+        // We better add that back in.
+        if (x == 0) {
+            bit_array[31] = '0';
+        }
+    }
+
+    // Reverse the order of the bit array.
+    for (x = 31; x >= 0; x--) {
+
+        // HACK: don't print out spaces!
+        if (bit_array[x] != ' ') {
+            printf("%c", bit_array[x]);
+
+            // This is to group the bits in groups of 4, like the example does.
+            if (count == 4) {
+                printf(" ");
+                count = 0;
+            }
+
+            count++;
+        }
+    }
+
+    printf("\n");
+
+    return;
 }
